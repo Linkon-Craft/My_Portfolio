9f311351ef4caa85a7c9940a12f4450ea2b9ee9f
linkon-craft <oyewoleolamilekan208@gmail.com>
2026-08-26 16:09:03 -0500

    Fix contact email configuration

--- config/settings.py
+++ config/settings.py
@@ -227,6 +227,23 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https',)
 # PRODUCTION SECURITY
 # =========================================================
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+
+EMAIL_PORT = 465
+
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CONTACT_EMAIL = os.getenv("CONTACT_EMAIL")
+
+
 if not DEBUG:
 
     SECURE_SSL_REDIRECT = True
