39acebe6291d5b7104f19f14c61b9cf61a40dbfa
linkon-craft <oyewoleolamilekan208@gmail.com>
2026-08-25 20:08:13 -0500

    done2

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'projects'
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path("project/<int:pk>/", views.project_detail, name="project_detail"),
     path('add_project/', views.add_project, name='add_project'),
     path("contact/", views.contact, name="contact"),
